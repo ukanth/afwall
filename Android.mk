6aaf221df715224fd782a3bc923bc0d631053928
Chris Aumann <me@chr4.org>
2013-12-19 17:36:55 +0000

    Add android-support-v4 to Android.mk
    Change-Id: Iaa1b486fecacf9327259027ef8ad48738f7fadac

--- Android.mk
+++ Android.mk
@@ -30,6 +30,9 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-support-v4
+
 LOCAL_PACKAGE_NAME := afwall
 LOCAL_CERTIFICATE := platform
 
