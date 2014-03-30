37141940ba4630b7de7ac4138398ee403019fe26
ukanth <cumakt@gmail.com>
2014-03-30 16:52:29 +0530

    old toggle is back with bug fix with password- I hate this

--- src/dev/ukanth/ufirewall/StatusWidget.java
+++ src/dev/ukanth/ufirewall/StatusWidget.java
@@ -23,13 +23,22 @@
 
 package dev.ukanth.ufirewall;
 
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+import dev.ukanth.ufirewall.RootShell.RootCommand;
 
 /**
  * ON/OFF Widget implementation
  */
-@Deprecated
-public class StatusWidget extends AppWidgetProvider {/*
+public class StatusWidget extends AppWidgetProvider {
 	@Override
 	public void onReceive(final Context context, final Intent intent) {
 		super.onReceive(context, intent);
@@ -44,11 +53,15 @@ public class StatusWidget extends AppWidgetProvider {/*
 			}
 		} else if (Api.TOGGLE_REQUEST_MSG.equals(intent.getAction())) {
 			// Broadcast sent to request toggling DroidWall's status
-			final SharedPreferences prefs = context.getSharedPreferences(Api.PREF_FIREWALL_STATUS, 0);
+			
 			final SharedPreferences prefs2 = context.getSharedPreferences(Api.PREFS_NAME, 0);
+			final String oldPwd = prefs2.getString(Api.PREF_PASSWORD, "");
+			final String newPwd = context.getSharedPreferences(Api.PREF_FIREWALL_STATUS, 0).getString("LockPassword", "");
+			
+			final SharedPreferences prefs = context.getSharedPreferences(Api.PREF_FIREWALL_STATUS, 0);
 			final boolean enabled = !prefs.getBoolean(Api.PREF_ENABLED, true);
-			final String pwd = prefs2.getString(Api.PREF_PASSWORD, "");
-			if (!enabled && pwd.length() != 0) {
+			
+			if (!enabled && oldPwd.length() == 0 && newPwd.length() == 0) {
 				Toast.makeText(context,R.string.widget_disable_fail,Toast.LENGTH_SHORT).show();
 				return;
 			}
@@ -91,12 +104,11 @@ public class StatusWidget extends AppWidgetProvider {/*
 			int[] widgetIds, boolean enabled) {
 		final RemoteViews views = new RemoteViews(context.getPackageName(),R.layout.onoff_widget);
 		final int iconId = enabled ? R.drawable.widget_on: R.drawable.widget_off;
-		views.setImageViewResource(R.id.widgetCanvas, iconId);
-
+		views.setInt(R.id.widgetCanvas, "setBackgroundResource", iconId);
 		final Intent msg = new Intent(Api.TOGGLE_REQUEST_MSG);
 		final PendingIntent intent = PendingIntent.getBroadcast(context, -1,msg, PendingIntent.FLAG_UPDATE_CURRENT);
 		views.setOnClickPendingIntent(R.id.widgetCanvas, intent);
 		manager.updateAppWidget(widgetIds, views);
 	}
 
-*/}
+}
