b15c31f016468d91ba3529bbaeb6670a1742b67d
ukanth <cumakt@gmail.com>
2014-11-19 23:59:18 +0530

    Moved from eclipse to Android Studio

---
+++ androidlockpattern2/build/generated/source/buildConfig/debug/com/haibison/android/lockpattern/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.haibison.android.lockpattern;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "com.haibison.android.lockpattern";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
