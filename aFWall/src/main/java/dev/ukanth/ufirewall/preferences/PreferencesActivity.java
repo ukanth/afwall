9006d0e763d5fc1bc453b3c75a1d5aec2143675d
ukanth <cumakt@gmail.com>
2015-05-05 22:07:43 +0530

    UI theming for preference/rename

--- aFWall/src/main/java/dev/ukanth/ufirewall/preferences/PreferencesActivity.java
+++ aFWall/src/main/java/dev/ukanth/ufirewall/preferences/PreferencesActivity.java
@@ -34,7 +34,13 @@ import android.preference.CheckBoxPreference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.support.v7.widget.AppCompatCheckBox;
+import android.support.v7.widget.AppCompatCheckedTextView;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.AppCompatRadioButton;
+import android.support.v7.widget.AppCompatSpinner;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -80,6 +86,36 @@ public class PreferencesActivity extends PreferenceActivity implements SharedPre
 				finish();
 			}
 		});
+
+
+	}
+
+	@Override
+	public View onCreateView(String name, Context context, AttributeSet attrs) {
+		// Allow super to try and create a view first
+		final View result = super.onCreateView(name, context, attrs);
+		if (result != null) {
+			return result;
+		}
+
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+			// If we're running pre-L, we need to 'inject' our tint aware Views in place of the
+			// standard framework versions
+			switch (name) {
+				case "EditText":
+					return new AppCompatEditText(this, attrs);
+				case "Spinner":
+					return new AppCompatSpinner(this, attrs);
+				case "CheckBox":
+					return new AppCompatCheckBox(this, attrs);
+				case "RadioButton":
+					return new AppCompatRadioButton(this, attrs);
+				case "CheckedTextView":
+					return new AppCompatCheckedTextView(this, attrs);
+			}
+		}
+
+		return null;
 	}
 
 
