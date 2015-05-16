68a11db3808855984068fc5723d589be1086aa33
ukanth <cumakt@gmail.com>
2015-05-16 09:07:03 +0530

    material tab for about help

--- aFWall/src/main/java/dev/ukanth/ufirewall/ui/about/AboutFragment.java
+++ aFWall/src/main/java/dev/ukanth/ufirewall/ui/about/AboutFragment.java
@@ -1,29 +1,43 @@
 package dev.ukanth.ufirewall.ui.about;
 
-import java.io.IOException;
-
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
 import dev.ukanth.ufirewall.Api;
-import dev.ukanth.ufirewall.util.G;
 import dev.ukanth.ufirewall.R;
+import dev.ukanth.ufirewall.util.G;
 
 
 public class AboutFragment extends Fragment {
+
+
+	private static final int SWIPE_MIN_DISTANCE = 120;
+	private static final int SWIPE_MAX_OFF_PATH = 250;
+	private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+	private GestureDetector gestureDetector;
+	View.OnTouchListener gestureListener;
+
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup group,
 			Bundle saved) {
 		View view = inflater.inflate(R.layout.help_about_content, group, false);
-		
+
+		ActivitySwipeDetector swipe = new ActivitySwipeDetector();
+		view.findViewById(R.id.about_thirdsparty_credits).setOnTouchListener(swipe);
 		return view;
 	}
 
@@ -54,5 +68,82 @@ public class AboutFragment extends Fragment {
 		} catch (IOException ioe) {
 			Log.e(Api.TAG, "Error reading changelog file!", ioe);
 		}
-	}		
+	}
+
+	interface SwipeInterface {
+
+		public void bottom2top(View v);
+
+		public void left2right(View v);
+
+		public void right2left(View v);
+
+		public void top2bottom(View v);
+	}
+
+
+	class ActivitySwipeDetector implements View.OnTouchListener {
+		static final String logTag = "ActivitySwipeDetector";
+		static final int MIN_DISTANCE = 100;
+		private float downX, downY, upX, upY;
+
+		public ActivitySwipeDetector() {
+		}
+
+		public void onRightToLeftSwipe(View v) {
+			Log.i(logTag, "RightToLeftSwipe!");
+		}
+
+		public void onLeftToRightSwipe(View v){
+			Log.i(logTag, "LeftToRightSwipe!");
+		}
+
+		public void onTopToBottomSwipe(View v){
+			Log.i(logTag, "onTopToBottomSwipe!");
+		}
+
+		public void onBottomToTopSwipe(View v){
+			Log.i(logTag, "onBottomToTopSwipe!");
+			Toast.makeText(getActivity(),"Swipe Works great",Toast.LENGTH_LONG).show();
+		}
+
+		public boolean onTouch(View v, MotionEvent event) {
+			switch(event.getAction()){
+				case MotionEvent.ACTION_DOWN: {
+					downX = event.getX();
+					downY = event.getY();
+					return true;
+				}
+				case MotionEvent.ACTION_UP: {
+					upX = event.getX();
+					upY = event.getY();
+
+					float deltaX = downX - upX;
+					float deltaY = downY - upY;
+
+					// swipe horizontal?
+					if(Math.abs(deltaX) > MIN_DISTANCE){
+						// left or right
+						if(deltaX < 0) { this.onLeftToRightSwipe(v); return true; }
+						if(deltaX > 0) { this.onRightToLeftSwipe(v); return true; }
+					}
+					else {
+						Log.i(logTag, "Swipe was only " + Math.abs(deltaX) + " long, need at least " + MIN_DISTANCE);
+					}
+
+					// swipe vertical?
+					if(Math.abs(deltaY) > MIN_DISTANCE){
+						// top or down
+						if(deltaY < 0) { this.onTopToBottomSwipe(v); return true; }
+						if(deltaY > 0) { this.onBottomToTopSwipe(v); return true; }
+					}
+					else {
+						Log.i(logTag, "Swipe was only " + Math.abs(deltaX) + " long, need at least " + MIN_DISTANCE);
+						v.performClick();
+					}
+				}
+			}
+			return false;
+		}
+	}
 }
--- aFWall/src/main/java/dev/ukanth/ufirewall/ui/about/SlidingTabLayout.java
+++ aFWall/src/main/java/dev/ukanth/ufirewall/ui/about/SlidingTabLayout.java
@@ -27,9 +27,7 @@ public class SlidingTabLayout extends HorizontalScrollView {
      * {@link #setCustomTabColorizer(TabColorizer)}.
      */
     public interface TabColorizer {
-
         int getIndicatorColor(int position);
-
     }
 
     private static final int TITLE_OFFSET_DIPS = 24;
