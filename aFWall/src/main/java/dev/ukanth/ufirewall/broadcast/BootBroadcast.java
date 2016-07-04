65c313033227b8e5d6b8f7c09c30d0fd1966d2b1
ukanth <cumakt@gmail.com>
2016-07-04 17:43:16 +0530

    cleanup the code

--- aFWall/src/main/java/dev/ukanth/ufirewall/broadcast/BootBroadcast.java
+++ aFWall/src/main/java/dev/ukanth/ufirewall/broadcast/BootBroadcast.java
@@ -41,65 +41,10 @@ import dev.ukanth.ufirewall.util.G;
  */
 public class BootBroadcast extends BroadcastReceiver {
 	// private Handler mHandler = new Handler(Looper.getMainLooper());
-	private final int id = 1;
-	private NotificationManager notificationManager;
-	private NotificationCompat.Builder notiBuilder;
+
 
 	@Override
 	public void onReceive(final Context context, final Intent intent) {
-
-		//hard code 5 seconds delay before apply rules
-		/*final int delay = 3;
-
-		notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-		notiBuilder = new NotificationCompat.Builder(context);
-		notiBuilder.setContentTitle(context.getString(R.string.applying_rules))
-				.setContentText(context.getString(R.string.apply_sequence, delay))
-				.setSmallIcon(R.drawable.notification_warn);
-
-		TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-		stackBuilder.addParentStack(MainActivity.class);
-		stackBuilder.addNextIntent(new Intent(context, MainActivity.class));
-		PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-		notiBuilder.setContentIntent(pendingIntent);
-
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				boolean notification = true;
-				for (int i = delay; i >= 0; i--)
-					try {
-						Thread.sleep(1000);
-						if (notification) {
-							notiBuilder.setContentText(context.getString(R.string.working)).setProgress(delay, delay - i, false);
-							notificationManager.notify(id, notiBuilder.build());
-						} else if ((i % 10 == 0 || i == delay) && i != 0) {
-						}
-					} catch (InterruptedException e) {
-					}
-				if (notification) {
-					notiBuilder.setContentText(context.getString(R.string.rules_applied)).setProgress(0, 0, false);
-					notificationManager.notify(id, notiBuilder.build());
-				}
-
-				InterfaceTracker.applyRulesOnChange(context, InterfaceTracker.BOOT_COMPLETED);
-
-				if(G.activeNotification()){
-					Api.showNotification(Api.isEnabled(context), context);
-				}
-				//make sure nflog starts after boot
-				if(G.enableLog() && "NFLOG".equals(G.logTarget())) {
-					context.startService(new Intent(context.getApplicationContext(), NflogService.class));
-				}
-				if (G.enableLogService()) {
-					context.startService(new Intent(context, LogService.class));
-				}
-				//cleanup the notification after applying rules
-				notificationManager.cancel(id);
-			}
-		}).start(); */
-
-		//TODO:  Old way of applying rules for now
 
 		InterfaceTracker.applyRulesOnChange(context, InterfaceTracker.BOOT_COMPLETED);
 
