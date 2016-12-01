0d61ff46d0c2b9e04c5ebd1c45713cbadf03e35e
ukanth <cumakt@gmail.com>
2016-12-01 07:30:01 +0530

    Replace dialog with MD in profiles activity

--- aFWall/src/main/java/dev/ukanth/ufirewall/activity/ProfileActivity.java
+++ aFWall/src/main/java/dev/ukanth/ufirewall/activity/ProfileActivity.java
@@ -1,16 +1,16 @@
 package dev.ukanth.ufirewall.activity;
 
-import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
+
+import com.afollestad.materialdialogs.MaterialDialog;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,7 +23,7 @@ import dev.ukanth.ufirewall.util.ProfileAdapter;
 /**
  * Created by ukanth on 31/7/15.
  */
-public class ProfileActivity extends AppCompatActivity{
+public class ProfileActivity extends AppCompatActivity {
     List<Profile> profilesList = new ArrayList<Profile>();
     ProfileAdapter profileAdapter;
 
@@ -79,8 +79,8 @@ public class ProfileActivity extends AppCompatActivity{
                                     ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
         AdapterView.AdapterContextMenuInfo aInfo = (AdapterView.AdapterContextMenuInfo) menuInfo;
-        Profile profile =  profileAdapter.getItem(aInfo.position);
-        menu.setHeaderTitle(getString(R.string.select) +  " " + profile.getProfileName());
+        Profile profile = profileAdapter.getItem(aInfo.position);
+        menu.setHeaderTitle(getString(R.string.select) + " " + profile.getProfileName());
         menu.add(1, 1, 1, getString(R.string.delete));
 
     }
@@ -90,7 +90,7 @@ public class ProfileActivity extends AppCompatActivity{
     public boolean onContextItemSelected(MenuItem item) {
         int itemId = item.getItemId();
         AdapterView.AdapterContextMenuInfo aInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-        if(aInfo.position > 3) {
+        if (aInfo.position > 3) {
             G.removeAdditionalProfile(profilesList.get(aInfo.position).getProfileName());
             profilesList.remove(aInfo.position);
             profileAdapter.notifyDataSetChanged();
@@ -109,8 +109,8 @@ public class ProfileActivity extends AppCompatActivity{
         profilesList.add(new Profile(G.gPrefs.getString("profile3", getString(R.string.profile3))));
 
         List<String> pList = G.getAdditionalProfiles();
-        for(String profileName : pList) {
-            if(profileName !=null && profileName.length() > 0) {
+        for (String profileName : pList) {
+            if (profileName != null && profileName.length() > 0) {
                 profilesList.add(new Profile(profileName));
             }
         }
@@ -120,23 +120,20 @@ public class ProfileActivity extends AppCompatActivity{
 
     // Handle user click
     public void addNewProfile() {
-        final Dialog d = new Dialog(this);
-        d.setContentView(R.layout.profile_adddialog);
-        d.setTitle(getString(R.string.profile_add));
-        d.setCancelable(true);
 
-        final EditText edit = (EditText) d.findViewById(R.id.editTextProfile);
-        Button b = (Button) d.findViewById(R.id.button1);
-        b.setOnClickListener(new View.OnClickListener() {
+        new MaterialDialog.Builder(this)
+                .cancelable(true)
+                .title(R.string.profile_add)
+                .inputType(InputType.TYPE_CLASS_TEXT)
+                .input(R.string.profile_add, R.string.profile_hint, new MaterialDialog.InputCallback() {
+                    @Override
+                    public void onInput(MaterialDialog dialog, CharSequence input) {
+                        String profileName = input.toString();
+                        ProfileActivity.this.profilesList.add(new Profile(profileName));
+                        G.addAdditionalProfile(profileName);
+                        ProfileActivity.this.profileAdapter.notifyDataSetChanged(); // We notify the data model is changed
+                    }
+                }).show();
 
-            public void onClick(View v) {
-                String profileName = edit.getText().toString();
-                ProfileActivity.this.profilesList.add(new Profile(profileName));
-                G.addAdditionalProfile(profileName);
-                ProfileActivity.this.profileAdapter.notifyDataSetChanged(); // We notify the data model is changed
-                d.dismiss();
-            }
-        });
-        d.show();
     }
 }
