f43bc50958657ee4ec1dd74b33591e28c975332d
ukanth <cumakt@gmail.com>
2017-01-30 07:57:27 +0530

    align issues and few minor changes from last pull request

--- aFWall/src/main/java/dev/ukanth/ufirewall/Api.java
+++ aFWall/src/main/java/dev/ukanth/ufirewall/Api.java
@@ -30,6 +30,8 @@ import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -3043,6 +3045,13 @@ public final class Api {
         } catch (InterruptedException e) {
         }
         return hasRoot[0];
+    }
+
+    // Clipboard
+    public static void copyToClipboard(Context context, String val) {
+        ClipboardManager clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("label", val);
+        clipboard.setPrimaryClip(clip);
     }
 
 
--- aFWall/src/main/java/dev/ukanth/ufirewall/util/LogNetUtil.java
+++ aFWall/src/main/java/dev/ukanth/ufirewall/util/LogNetUtil.java
@@ -1,7 +1,5 @@
 package dev.ukanth.ufirewall.util;
 
-import android.content.ClipData;
-import android.content.ClipboardManager;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
@@ -22,34 +20,33 @@ import dev.ukanth.ufirewall.log.Log;
 import eu.chainfire.libsuperuser.Shell;
 
 /**
- * This file was created to simplify Network Function in AfWall+ log system
+ * This file was created to simplify Network Function in AFWall+ log system
  * Created by vzool on 1/20/17.
  */
 
 public class LogNetUtil {
 
-    final static String TAG = "afWall-LogNetUtil";
+    final static String TAG = "AFWall-LogNetUtil";
 
-    public static class NetTask extends AsyncTask<NetParam, Integer, String>
-    {
+    public static class NetTask extends AsyncTask<NetParam, Integer, String> {
         long start_time;
-        OnFinishRequest onFinishRequest_callback;
+        OnFinishRequest onFinishRequest;
         MaterialDialog progress;
         Context context;
         String output_result = "";
 
         private static final String PING_CMD = "%s ping -w 1 -W %d %s";
 
-        public NetTask(Context context){
+        public NetTask(Context context) {
             this.context = context;
         }
 
-        long finish_time(){
+        long finish_time() {
             return System.currentTimeMillis() - start_time;
         }
 
-        public NetTask setOnFinishRequest(OnFinishRequest when){
-            onFinishRequest_callback = when;
+        public NetTask setOnFinishRequest(OnFinishRequest when) {
+            onFinishRequest = when;
             return this;
         }
 
@@ -66,89 +63,53 @@ public class LogNetUtil {
         }
 
         @Override
-        protected String doInBackground(NetParam... params)
-        {
+        protected String doInBackground(NetParam... params) {
             start_time = System.currentTimeMillis();
-
-            try{
-
-                switch(params[0].type){
-
+            try {
+                switch (params[0].type) {
                     case PING:
-
                         // Ping
-
-                        try{
-
+                        try {
                             String shell_result = "";
                             String command = "";
-
-                            try{
-
+                            try {
                                 // This command needs permission to allow
                                 // AFWall+ itself to has access to network
                                 // to work probably
                                 command = String.format(PING_CMD, "", G.logPingTimeout(), params[0].address);
-
                                 Log.d(TAG, "Execute CMD: " + command);
                                 Process process = Runtime.getRuntime().exec(command);
-
-                                // wait command process to finish
                                 process.waitFor();
-
-                                // debug exit code
                                 Log.d(TAG, "CMD exit code: " + process.exitValue());
-
                                 // check if ping command does not encounter any errors
-                                if(process.exitValue() == 0){
-
+                                if (process.exitValue() == 0) {
                                     //The ping was succeeded.
                                     shell_result = parse(process);
-
-                                }else{
-
+                                } else {
                                     shell_result = su_busyboox_ping(params[0].address);
                                 }
-
-                            }catch (Exception ping_cmd_ex){
-
+                            } catch (Exception ping_cmd_ex) {
                                 Log.e(TAG, "Exception(00): " + ping_cmd_ex.getMessage());
-
                                 shell_result = su_busyboox_ping(params[0].address);
                             }
-
                             return shell_result;
-
-                        }catch (Exception eex){
-
+                        } catch (Exception eex) {
                             Log.e(TAG, "Exception(01): " + eex.getMessage());
-
                             // final choice is to use Android API
-
                             return normal_ping(params[0].address);
                         }
-
                     case RESOLVE:
-
                         // Resolve
-
-                        try{
-
+                        try {
                             return InetAddress.getByName(params[0].address).getCanonicalHostName();
-
-                        }catch(UnknownHostException ex){
-
+                        } catch (UnknownHostException ex) {
                             Log.e(TAG, "Exception(02): " + ex.getMessage());
-
                             return String.format("Currently can not resolve Host for IP(%s), timeout: %d ms", params[0].address, finish_time());
                         }
                 }
-
-            }catch (Exception e){
-
+            } catch (Exception e) {
                 Log.e(TAG, "Exception(03): " + e.getMessage());
             }
-
             return context.getString(R.string.error_or_unknown_category);
         }
 
@@ -156,8 +117,8 @@ public class LogNetUtil {
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
 
-            if(onFinishRequest_callback != null){
-                onFinishRequest_callback.then(s);
+            if (onFinishRequest != null) {
+                onFinishRequest.then(s);
             }
 
             try {
@@ -167,15 +128,13 @@ public class LogNetUtil {
             } catch (IllegalArgumentException e) {
                 Log.e(TAG, e.getMessage());
                 // Handle or log or ignore
-            } catch (final Exception e) {
+            } catch (Exception e) {
                 Log.e(TAG, e.getMessage());
                 // Handle or log or ignore
             } finally {
                 progress = null;
             }
-
             output_result = s;
-
             new MaterialDialog.Builder(context)
                     .content(s)
                     .title(R.string.result)
@@ -184,94 +143,65 @@ public class LogNetUtil {
                     .onPositive(new MaterialDialog.SingleButtonCallback() {
                         @Override
                         public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
-                            copyToClipboard(context, output_result);
+                            Api.copyToClipboard(context, output_result);
                             Api.toast(context, context.getString(R.string.result_copied_to_clipboard));
                         }
-                    })
-                    .show();
+                    }).show();
         }
 
-        String normal_ping(String ip){
-
+        private String normal_ping(String ip) {
             String result = "";
-
             try {
-
-                if(InetAddress.getByAddress(ip.getBytes()).isReachable(G.logPingTimeout() * 1000)){ // isReachable expect timeout in millisecond
-
+                if (InetAddress.getByAddress(ip.getBytes()).isReachable(G.logPingTimeout() * 1000)) { // isReachable expect timeout in millisecond
                     result = String.format(context.getString(R.string.reachable_timeout), finish_time());
                 }
-
             } catch (Exception e) {
-
                 Log.e(TAG, "Exception(04): " + e.getMessage());
-
                 result = String.format("Currently IP(%s) is not Reachable, timeout: %d ms", ip, finish_time());
             }
-
             return result;
         }
 
-        String su_busyboox_ping(String ip){
-
+        private String su_busyboox_ping(String ip) {
             // using libsuperuser to perform ping by Busybox,
             // This will need permission in AFWall+
             // "0:(root) Apps running as root"
-
             String result = "";
-
             String command = String.format(PING_CMD, Api.getBusyBoxPath(context, true), G.logPingTimeout(), ip);
-
             Log.d(TAG, "Execute CMD: " + command);
-
-            result = parse(Shell.run("su", new String[]{
-                    command
-            }, null, true));
-
-            if(result.isEmpty()){
+            result = parse(Shell.run("su", new String[]{command}, null, true));
+            if (result.isEmpty()) {
 
                 return context.getString(R.string.network_connection_not_available);
             }
-
             return result;
         }
 
-        String parse(List<String> output){
-
+        private String parse(List<String> output) {
             String result = "";
-
-            for(String line : output){
+            for (String line : output) {
                 result += line + " ";
             }
-
-            if(result.isEmpty()){
-
+            if (result.isEmpty()) {
                 return context.getString(R.string.output_is_empty);
             }
-
             return result;
         }
 
-        String parse(Process process){
-
+        private String parse(Process process) {
             try {
                 BufferedReader bufferedReader = new BufferedReader(
                         new InputStreamReader(process.getInputStream()));
-
                 // Grab the results
                 StringBuilder log = new StringBuilder();
                 String line;
                 while ((line = bufferedReader.readLine()) != null) {
                     log.append(line + "\n");
                 }
-
                 return log.toString();
-
             } catch (IOException e) {
-
                 Log.e(TAG, "Exception(05): " + e.getMessage());
             }
-
             return context.getString(R.string.output_is_empty);
         }
     }
@@ -286,21 +216,13 @@ public class LogNetUtil {
         public JobType type;
         public String address;
 
-        public NetParam(JobType type, String address){
+        public NetParam(JobType type, String address) {
             this.type = type;
             this.address = address;
         }
     }
 
-    // Clipboard
-
-    public static void copyToClipboard(Context context, String val){
-        ClipboardManager clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
-        ClipData clip = ClipData.newPlainText("label", val);
-        clipboard.setPrimaryClip(clip);
-    }
-
-    public interface OnFinishRequest{
+    public interface OnFinishRequest {
         void then(String result);
     }
 
