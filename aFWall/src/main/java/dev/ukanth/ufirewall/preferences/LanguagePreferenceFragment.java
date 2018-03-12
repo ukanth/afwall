1751cf63fa877b08a4c3f1235c7788f664fb8bc9
ukanth <cumakt@gmail.com>
2018-03-12 22:15:29 +0530

    minor mempry leak on preference

--- aFWall/src/main/java/dev/ukanth/ufirewall/preferences/LanguagePreferenceFragment.java
+++ aFWall/src/main/java/dev/ukanth/ufirewall/preferences/LanguagePreferenceFragment.java
@@ -12,48 +12,51 @@ import dev.ukanth.ufirewall.R;
 import dev.ukanth.ufirewall.util.G;
 
 public class LanguagePreferenceFragment extends PreferenceFragment implements
-		SharedPreferences.OnSharedPreferenceChangeListener
-	{
+        SharedPreferences.OnSharedPreferenceChangeListener {
 
-	private static CheckBoxPreference checkBoxPreference;
+    private static CheckBoxPreference checkBoxPreference;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		// Load the preferences from an XML resource
-		addPreferencesFromResource(R.xml.language_preferences);
-		checkXposed(findPreference("fixDownloadManagerLeak"));
-		//checkXposed(findPreference("lockScreenNotification"),this.getActivity().getApplicationContext());
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.language_preferences);
+        checkXposed(findPreference("fixDownloadManagerLeak"));
+        //checkXposed(findPreference("lockScreenNotification"),this.getActivity().getApplicationContext());
+    }
 
-	public static void checkXposed(Preference pref) {
-		if (pref == null) {
-			return;
-		}
-		checkBoxPreference = (CheckBoxPreference) pref;
-		// gray out the fixDownloadManagerLeak preference if xposed module is not activated
-		Log.i(Api.TAG,"Checking Xposed:" + G.isXposedEnabled() + "");
-		checkBoxPreference.setEnabled(G.isXposedEnabled());
-	}
+    public static void checkXposed(Preference pref) {
+        if (pref == null) {
+            return;
+        }
+        checkBoxPreference = (CheckBoxPreference) pref;
+        // gray out the fixDownloadManagerLeak preference if xposed module is not activated
+        Log.i(Api.TAG, "Checking Xposed:" + G.isXposedEnabled() + "");
+        checkBoxPreference.setEnabled(G.isXposedEnabled());
+    }
 
-	@Override
-	public void onResume() {
-		super.onResume();
-		getPreferenceManager().getSharedPreferences()
-				.registerOnSharedPreferenceChangeListener(this);
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceManager().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
 
-	}
+    @Override
+    public void onPause() {
+        getPreferenceManager().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+        super.onPause();
+    }
 
-	@Override
-	public void onPause() {
-		getPreferenceManager().getSharedPreferences()
-				.unregisterOnSharedPreferenceChangeListener(this);
-		super.onPause();
-	}
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        checkBoxPreference = null;
+    }
 
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
 
-	@Override
-	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-
-	}
+    }
 }
