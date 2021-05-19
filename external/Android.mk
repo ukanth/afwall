32c9a04e6e30d9971ab8835d509776243c7e9076
ukanth <165996+ukanth@users.noreply.github.com>
2021-05-19 21:51:52 +0530

    #1204: F-droid build issue

--- external/Android.mk
+++ external/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 ZPATH := $(LOCAL_PATH)
 
-include $(ZPATH)/nflog/Android.m
+include $(ZPATH)/nflog/Android.mk
 
 # run pie is no longer supported
 # include $(ZPATH)/run_pie/Android.mk
