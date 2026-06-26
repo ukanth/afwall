e7db2a25b9ce606787947ac037c7aeed8266a2b4
Mainkill1 <Mainkill1@gmail.com>
2026-06-26 03:42:50 -0700

    Recognize WireGuard and Bluetooth tether interfaces

--- app/src/main/java/dev/ukanth/ufirewall/InterfaceTracker.java
+++ app/src/main/java/dev/ukanth/ufirewall/InterfaceTracker.java
@@ -60,9 +60,9 @@ public final class InterfaceTracker {
             "wwan+", "cdma_rmnet+", "clat4+", "cc2mni+", "bond1+", "rmnet_smux+", "ccinet+",
             "v4-rmnet+", "seth_w+", "v4-rmnet_data+", "rmnet_ipa+", "rmnet_data+", "r_rmnet_data+"};
 
-    public static final String[] ITFS_VPN = {"tun+", "ppp+", "tap+"};
+    public static final String[] ITFS_VPN = {"tun+", "ppp+", "tap+", "wg+"};
 
-    public static final String[] ITFS_TETHER = {"bt-pan", "usb+", "rndis+", "rmnet_usb+"};
+    public static final String[] ITFS_TETHER = {"bt-pan", "bnep+", "usb+", "rndis+", "rmnet_usb+"};
 
     public static final String BOOT_COMPLETED = "BOOT_COMPLETED";
     public static final String CONNECTIVITY_CHANGE = "CONNECTIVITY_CHANGE";
